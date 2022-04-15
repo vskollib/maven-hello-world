f87ab7447c5ea704a33402c012d3762b80785648
Kishore <ind2usadreams@gmail.com>
2022-04-14 17:53:23 -0700

    updated the changes

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -4,11 +4,11 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Welcome to DevOps" );
+        System.out.println( "Hello World! Welcome to DevOps world" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
